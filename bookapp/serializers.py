7867c00ad7e29d5dbf9fb43adeeb4ac14f10ff1d
umidjonxikmatov <xikmatovumidjon1120@gmail.com>
2024-03-28 17:33:02 +0500

    first commit

--- bookapp/serializers.py
+++ bookapp/serializers.py
@@ -6,3 +6,14 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ('id', 'title', 'author', 'pages', 'price', 'cover_image', 'isbn')
+
+
+class BooksSerializers(serializers.Serializer):
+    title = serializers.CharField()
+    author = serializers.IntegerField()
+    pages = serializers.IntegerField()
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    cover_image = serializers.URLField()
+    isbn = serializers.CharField()
+
+
